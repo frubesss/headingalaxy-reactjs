fc306a72e2ea7c32995874523de3d54df3054ed0
frubesss <robertson.cra@gmail.com>
2017-07-03 20:40:27 +0100

    finished html canvas logic placing the users face inside the selected galaxy

--- src/Finish.jsx
+++ src/Finish.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { Card, CardMedia, CardActions } from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
 
-let galaxy1 = require('../public/images/1.jpg');
-let galaxy2 = require('../public/images/2.jpg');
+let galaxy1 = new Image();
+galaxy1.src = require('../public/images/1.jpg');
+
+let galaxy2 = new Image();
+galaxy2.src = require('../public/images/2.jpg');
+
+let cat = new Image();
+cat.src = require('../public/images/grumpy_cat.jpg');
 
 export default class Finish extends React.Component {
 
@@ -11,6 +17,11 @@ export default class Finish extends React.Component {
         super(props);
         this.handleSelectedGalaxy = this.handleSelectedGalaxy.bind(this);
         this.handleSelectedAnimation = this.handleSelectedAnimation.bind(this);
+        this.handleCombineSelections = this.handleCombineSelections.bind(this);
+    }
+
+    componentDidMount() {
+        this.handleCombineSelections();
     }
 
     handleSelectedGalaxy(){
@@ -20,6 +31,10 @@ export default class Finish extends React.Component {
         return galaxy2
     }
 
+    handleFaceUploaded(){
+
+    }
+
     handleSelectedAnimation(){
         if(this.props.activeAnimation === '1'){
             return galaxy1
@@ -27,16 +42,29 @@ export default class Finish extends React.Component {
         return galaxy2
     }
 
+    handleCombineSelections(){
+        const ctx = this.refs.canvas.getContext('2d');
+        const canvas = document.getElementById('canvas');
+
+        let galaxyImage = this.handleSelectedGalaxy();
+
+        ctx.drawImage(galaxyImage, 0, 0, galaxyImage.width, galaxyImage.height);
+        ctx.drawImage(cat, canvas.width / 2 - cat.width / 2,
+            canvas.height / 2 - cat.height / 2, 150, 150);
+    }
+
     render() {
         return (
-            <Card>
-                <CardMedia>
-                    <img src={this.handleSelectedGalaxy()} alt="selected-galaxy" />
-                </CardMedia>
-                <CardActions>
-                    <RaisedButton label="Download" primary={true} />
-                </CardActions>
-            </Card>
+            <div>
+                <Card>
+                    <CardMedia>
+                        <canvas id="canvas" ref="canvas" width="1920" height="1080"/>
+                    </CardMedia>
+                    <CardActions>
+                        <RaisedButton label="Download" primary={true} />
+                    </CardActions>
+                </Card>
+            </div>
         );
     }
 }
